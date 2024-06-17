c54ee2ae1a15b3a606169ec3c3e6b48dab42d2f6
Bin Bin <phuduthang300502@gmail.com>
2024-06-17 10:22:22 +0700

    SEO ENGINE

--- a_posts/models.py
+++ a_posts/models.py
@@ -30,6 +30,9 @@ class Post(models.Model):
     class Meta:
         ordering = ["-created"]
 
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
 
 class LikedPost(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -51,6 +54,9 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ["order"]
+
+    def get_absolute_url(self):
+        return f"/category/{self.slug}/"
 
 
 class Comment(models.Model):
